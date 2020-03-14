839ca5ebc10a6e9b6e267ca4f2179140ef8d8ff8
Eina Onting <einers@outlook.com>
2020-03-14 00:49:43 -0700

    clean up unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ??? aaaaaa ????? Edit <code>src/App.tsx</code> and save to reload. Aaaaaaaaaaaaa.
+          Edit <code>src/App.tsx</code> and save to reload
         </p>
         <a
           className="App-link"
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,5 @@
-// manual proxy for firefox hot reloading: https://github.com/facebook/create-react-app/issues/6720#issuecomment-486641448
+// manual proxy for firefox hot reloading:
+// https://github.com/facebook/create-react-app/issues/6720#issuecomment-486641448
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {

d2179cd0bce4e88c2ee6d2b90f89ed55da332562
Eina Onting <einers@outlook.com>
2020-03-14 01:03:29 -0700

    setup ports + proxy for api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8001",
+      target: "http://localhost:3000",
       changeOrigin: true
     })
   );

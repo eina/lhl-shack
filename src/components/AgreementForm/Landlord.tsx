a06d1f8066425055cca67328dcdb9454ca601ebc
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-16 20:04:30 -0700

    add all fields for landlord form component

--- src/components/AgreementForm/Landlord.tsx
+++ src/components/AgreementForm/Landlord.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Field } from "formik";
 
 const Landlord = () => {
-  return <Field type="text" name="address" placeholder="Address" />;
+  return (
+  
+  <div>
+  <Field type="text" name="landlord-name" placeholder="Full Name" required/>;
+  <Field type="text" name="address" placeholder="Address" required/>;
+  <Field type="text" name="email" placeholder="Email" required/>;
+  <Field type="text" name="phone" placeholder="Phone Number" required/>;
+  <Field type="text" name="company" placeholder="Property Management Company (if applicable)" />;
+  </div>
+  )
 };
 
 export default Landlord;

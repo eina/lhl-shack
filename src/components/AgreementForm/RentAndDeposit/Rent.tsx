65012b05de9973239f9fca38da4676a7839362d3
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-19 11:58:35 -0700

    add next section button to Roommates and Bills Rent

--- src/components/AgreementForm/RentAndDeposit/Rent.tsx
+++ src/components/AgreementForm/RentAndDeposit/Rent.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import SplitRentAndDeposit from "./SplitRentAndDeposit";
+import SplitRentAndDeposit from './SplitRentAndDeposit';
+
+import { Button } from '@chakra-ui/core';
+
+import { useHistory } from 'react-router-dom';
 
 const Rent = (props: any) => {
+  const history = useHistory();
   return (
     <>
       <h2>Rent</h2>
       <SplitRentAndDeposit sectionName="rent" {...props} />
+      <div>
+        <Button
+          variantColor="pink"
+          onClick={() => {
+            history.push('/agreement/bills/deposit');
+          }}
+        >
+          Next Section
+        </Button>
+      </div>
     </>
   );
 };
--- src/components/AgreementForm/Roommates.tsx
+++ src/components/AgreementForm/Roommates.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import { FieldArray, useFormikContext } from "formik";
+import React from 'react';
+import { FieldArray, useFormikContext } from 'formik';
 
-import { FormValues } from "../../interfaces";
+import { FormValues } from '../../interfaces';
 
-import FieldSet from "../FieldSet";
+import FieldSet from '../FieldSet';
+
+import { Button } from '@chakra-ui/core';
+
+import { useHistory } from 'react-router-dom';
 
 const Roommates = () => {
+  const history = useHistory();
   const { values }: { values: FormValues } = useFormikContext();
   return (
     <div>
@@ -16,10 +21,26 @@ const Roommates = () => {
             <ol>
               {values.roommates.map((roomie, index) => (
                 <li key={index}>
-                  <FieldSet type="text" name={`roommates.${index}.firstName`} label="First Name" />
-                  <FieldSet type="text" name={`roommates.${index}.lastName`} label="Last Name" />
-                  <FieldSet type="text" name={`roommates.${index}.email`} label="Email" />
-                  <FieldSet type="text" name={`roommates.${index}.phone`} label="Phone" />
+                  <FieldSet
+                    type="text"
+                    name={`roommates.${index}.firstName`}
+                    label="First Name"
+                  />
+                  <FieldSet
+                    type="text"
+                    name={`roommates.${index}.lastName`}
+                    label="Last Name"
+                  />
+                  <FieldSet
+                    type="text"
+                    name={`roommates.${index}.email`}
+                    label="Email"
+                  />
+                  <FieldSet
+                    type="text"
+                    name={`roommates.${index}.phone`}
+                    label="Phone"
+                  />
                   <p>======== temporary line break ============</p>
                 </li>
               ))}
@@ -27,7 +48,12 @@ const Roommates = () => {
             <button
               type="button"
               onClick={() =>
-                arrayHelpers.push({ firstName: "", lastName: "", email: "", phone: "" })
+                arrayHelpers.push({
+                  firstName: '',
+                  lastName: '',
+                  email: '',
+                  phone: '',
+                })
               }
             >
               Add Roommate
@@ -35,6 +61,16 @@ const Roommates = () => {
           </div>
         )}
       </FieldArray>
+      <div>
+        <Button
+          variantColor="pink"
+          onClick={() => {
+            history.push('/agreement/bills/rent');
+          }}
+        >
+          Next Section
+        </Button>
+      </div>
     </div>
   );
 };

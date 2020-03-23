fc850026116dd970d91f6599720ec43fcc4dc52a
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-23 12:36:58 -0700

    add external links

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Heading } from '@chakra-ui/core';
+import { Heading, Link } from '@chakra-ui/core';
 
 const Resources = () => {
   return (
@@ -8,7 +8,22 @@ const Resources = () => {
       <Heading as="h1">Resources</Heading>
       <div>
         {/* <Heading as="h3"></Heading> */}
-        <a href="https://wiki.clicklaw.bc.ca/index.php/Tenant_Survival_Guide">Tenant Survival Guide Wikibook</a>
+        <Link color="teal.500" href="https://wiki.clicklaw.bc.ca/index.php/Tenant_Survival_Guide" isExternal>Tenant Survival Guide Wikibook</Link>
+      </div>
+      <div>
+        <Link color="teal.500" href="http://tenants.bc.ca/template-letters/" isExternal>Template Letters</Link>
+      </div>
+      <div>
+        <Link color="teal.500" href="http://www.accessprobono.ca/residential-tenancy-program" isExternal>Access Pro Bono – Residential Tenancy Program</Link>
+      </div>
+      <div>
+        <Link color="teal.500" href="http://tenants.bc.ca/tenant-infoline/" isExternal>Tenant Info Line</Link>
+      </div>
+      <div>
+        <Link color="teal.500" href="http://tenants.bc.ca/renting-it-right/" isExternal>FREE! Renting It Right Course</Link>
+      </div>
+      <div>
+        <Link color="teal.500" href="http://tenants.bc.ca/am-i-covered-by-the-law/" isExternal>Residential Tenancy Act Info</Link>
       </div>
     </div>
   );

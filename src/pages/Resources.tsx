9e8855c8cf29d52dd3893489be2641237205394e
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-23 12:20:07 -0700

    add first link

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import { Heading } from '@chakra-ui/core';
 
 const Resources = () => {
   return (
     <div>
-      Resources
+      <Heading as="h1">Resources</Heading>
+      <div>
+        {/* <Heading as="h3"></Heading> */}
+        <a href="https://wiki.clicklaw.bc.ca/index.php/Tenant_Survival_Guide">Tenant Survival Guide Wikibook</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Resources;

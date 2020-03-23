0a61207238f5d99f87badccd8f92aa7ebddc6d88
Eina Onting <einers@outlook.com>
2020-03-23 02:12:42 -0700

    test html string rendering with reactdomserver

--- src/components/AgreementForm/AgreementPreview.tsx
+++ src/components/AgreementForm/AgreementPreview.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import ReactDOMServer from 'react-dom/server';
+import { useFormikContext } from 'formik';
+import { Heading, Box, List, ListItem } from '@chakra-ui/core';
+import { displayFullName } from '../../helpers/functions';
 
+const LandlordPreview = ({landlord}: {landlord: any}) => {
+  console.log(landlord);
+  return (
+    <Box as="section">
+      <Heading as="h1">Landlord</Heading>
+      <List as="dl">
+        <ListItem as="div">
+          <dt>Name</dt>
+          <dd>{displayFullName(landlord.firstName, landlord.lastName)}</dd>
+        </ListItem>
+
+        <ListItem as="div">
+          <dt>Phone</dt>
+          <dd>{landlord.phone}</dd>
+        </ListItem>
+
+        <ListItem as="div">
+          <dt>Email</dt>
+          <dd>{landlord.email}</dd>
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
+
+// add date_saved column
 const AgreementPreview = () => {
-  return <div>hi</div>;
+  const { values, values: { landlord, household, account, roommates, rent, securityDeposit, bills, housekeeping, signatures} } = useFormikContext();
+
+  const landlordString = ReactDOMServer.renderToStaticMarkup(<LandlordPreview landlord={landlord}/>);
+  // it woooooooooooooorks
+  console.log('does this work', landlordString);
+
+  return (
+    <Box>
+      <Heading as="h1">Rommate Agreement</Heading>
+
+      <LandlordPreview landlord={landlord} />
+      {/* {JSON.stringify(values)} */}
+    </Box>
+  );
 };
 
 export default AgreementPreview;

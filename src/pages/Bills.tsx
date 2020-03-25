cdaa02b46208648318a8fc97654b3ab5857e695b
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-24 19:00:49 -0700

    add title to bills page

--- src/pages/Bills.tsx
+++ src/pages/Bills.tsx
@@ -2,24 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import {Column, Table} from 'react-virtualized';
 import { Cell, Column, Table } from '@blueprintjs/table';
-import '../../node_modules/@blueprintjs/table/lib/css/table.css'
-//Table data as an array of objects
-const list = [
-  { name: 'Clare Nolan', description: 'Software engineer' },
-  // And so on...
-];
-// have two ids, one for the row, one for the column when grabbing API data - to help sort
+import '../../node_modules/@blueprintjs/table/lib/css/table.css';
+
+import { Heading } from '@chakra-ui/core';
+
+// Mentor tip: have two ids, one for the row, one for the column when grabbing API data - to help sort
 const cellRenderer = (rowIndex: number) => {
-  return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>;
+  return <Cell>{`static content`}</Cell>;
 };
 const Bills = () => {
-  // Render your table
-
   return (
+    <div>
+      <Heading as="h1">Bills</Heading>
       <Table numRows={10}>
         <Column name="Name" cellRenderer={cellRenderer} />
         <Column name="Due Date" cellRenderer={cellRenderer} />
+        <Column name="Total" cellRenderer={cellRenderer} />
+        <Column name="Your Total" cellRenderer={cellRenderer} />
+        <Column name="Bill Status" cellRenderer={cellRenderer} />
+        <Column name="Your Payment Status" cellRenderer={cellRenderer} />
+        <Column name="Roomie Payment Status" cellRenderer={cellRenderer} />
       </Table>
+    </div>
   );
 };
 

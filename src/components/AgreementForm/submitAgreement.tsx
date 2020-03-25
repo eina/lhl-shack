14b0433e9257de70eec483ccb57d270d8faf45ba
Eina Onting <einers@outlook.com>
2020-03-24 18:42:45 -0700

    refactor saveUser function

--- src/components/AgreementForm/submitAgreement.tsx
+++ src/components/AgreementForm/submitAgreement.tsx
@@ -43,6 +43,41 @@ const saveAgreement = ({ formVals, householdID, agreementID, isComplete }: Agree
   );
 };
 
+type SaveUsersProps = {
+  formVals: any;
+};
+const saveUsers = ({ formVals }: SaveUsersProps) => {
+  // save the users
+  // check if the users exist first
+  const usersPromiseArray = formVals.roommates.map((roomie: any) =>
+    // stop the whole thing here if the user emails are the same
+    axios.get(`/api/users?email=${roomie.email}`)
+  );
+  return Promise.all(usersPromiseArray).then(usersData => {
+    return usersData.map((user: any, index: number) => {
+      // if user doesn't exist
+      if (user.data.length === 0) {
+        return axios
+          .post(`/api/users/`, {
+            ...formVals.roommates[index],
+            password: uuidV4()
+          })
+          .then(createdUser => {
+            return createdUser.data.id;
+          });
+      } else {
+        return axios
+          .patch(`/api/users/${user.data[0].id}`, {
+            ...formVals.roommates[index]
+          })
+          .then(editedUser => {
+            return editedUser.data.id;
+          });
+      }
+    });
+  });
+};
+
 const submitAgreement = ({ formVals, householdID, agreementID, isComplete }: AgreementProps) => {
   const { housekeeping } = formVals;
   const formattedValues = {
@@ -56,38 +91,10 @@ const submitAgreement = ({ formVals, householdID, agreementID, isComplete }: Agr
       // only save the other things when submitting from the agreement form (isComplete == true)
       if (isComplete) {
         // save the users
-        // check if the users exist first
-        const usersPromiseArray = formVals.roommates.map((roomie: any) =>
-          // stop the whole thing here if the user emails are the same
-          axios.get(`/api/users?email=${roomie.email}`)
-        );
-        return Promise.all(usersPromiseArray)
-          .then(usersData => {
-            return usersData.map((user: any, index: number) => {
-              // if user doesn't exist
-              if (user.data.length === 0) {
-                return axios
-                  .post(`/api/users/`, {
-                    ...formVals.roommates[index],
-                    password: uuidV4()
-                  })
-                  .then(createdUser => {
-                    return createdUser.data.id;
-                  });
-              } else {
-                return axios
-                  .patch(`/api/users/${user.data[0].id}`, {
-                    ...formVals.roommates[index]
-                  })
-                  .then(editedUser => {
-                    return editedUser.data.id;
-                  });
-              }
-            });
-          })
+        return saveUsers({ formVals })
           .then(users => Promise.all(users)) // grab users id
           .then(usersIDs => {
-            // account for deleting == formVals.bills is not gonna be the same length as the server (grab it per household and count the keys?)
+            console.log("excuse me where are my users", usersIDs);
             // 1. loop through formvals to double check that you're not recreating a bill that's already made (through bill_uuid)
             return formVals.bills.map((bill: any) => {
               const billToSend = {
@@ -102,20 +109,20 @@ const submitAgreement = ({ formVals, householdID, agreementID, isComplete }: Agr
                   .get("/api/bills", {
                     params: {
                       bill_uuid: bill.bill_uuid,
-                      household_id: householdID
-                      // user_id: userID
+                      household_id: householdID,
+                      user_id: userID
                     }
                   })
                   .then(houseBillPerUser => {
                     // check if bill exists for the user or not
                     if (!houseBillPerUser.data.length) {
-                      console.log(`creating ${bill.bill_uuid} for userID`);
+                      console.log(`creating ${bill.bill_uuid} for ${userID}`);
                       return axios.post("/api/bills/", {
                         ...billToSend,
                         user_id: userID
                       });
                     } else {
-                      console.log(`updating (!?!) ${bill.bill_uuid} for userID`);
+                      console.log(`updating (!?!) ${bill.bill_uuid} for ${userID}`);
                       // loop through the created bills with that bill uuidhouseBillPerUser.data
                       return houseBillPerUser.data.map((billToUpdate: any) => {
                         return axios.patch(`/api/bills/${billToUpdate.id}`, {
@@ -127,34 +134,7 @@ const submitAgreement = ({ formVals, householdID, agreementID, isComplete }: Agr
                   });
               });
             });
-
-            // return axios.get(`/api/bills?household_id=${householdID}`).then(houseBills => {
-            //   // if there are no bills
-            //   // make a bill, loop through the usersIDs
-            //   if (!houseBills.data.length || houseBills.data.length !== formVals.bills.length) {
-            //     return formVals.bills.map((bill: any) => {
-            //       const billToSend = {
-            //         ...bill,
-            //         total_amount: bill.total_amount * 1,
-            //         interval: bill.interval.value,
-            //         household_id: householdID
-            //       };
-            //       return usersIDs.map(userID => {
-            //         console.log("creating a bill for", userID);
-            //         return axios.post("/api/bills/", { ...billToSend, user_id: userID });
-            //       });
-            //     });
-            //   } else {
-            //     // have list of bills,
-            //     // since am
-            //     const billIdentifiers = houseBills.data.map((bill: any) => ({
-            //       bill_id: bill.id,
-            //       bill_uuid: bill.bill_uuid
-            //     }));
-            //     console.log("something", billIdentifiers);
-            //   }
-            // });
-          }); // return vals.data;
+          });
       } else {
         return vals.data;
       }

39393dcafd042abad6a5805b02436662918fd364
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-26 14:32:40 -0700

    add column names and selectors

--- src/pages/Bills.tsx
+++ src/pages/Bills.tsx
@@ -1,21 +1,19 @@
 import React, { useEffect, useContext, useState } from 'react';
-// import { Cell, Column, Table } from '@blueprintjs/table';
-// import '../../node_modules/@blueprintjs/table/lib/css/table.css';
-
-import Griddle, { plugins } from 'griddle-react';
-
-import { Heading } from '@chakra-ui/core';
 import axios from 'axios';
-
+import Select from 'react-select';
+import Griddle, {
+  plugins,
+  RowDefinition,
+  ColumnDefinition,
+} from 'griddle-react';
+import { Heading } from '@chakra-ui/core';
 import { AppContext } from '../Store';
-
 
 const Bills = () => {
   const {
     state,
     updateState,
   }: { state: any; updateState: Function } = useContext(AppContext);
-  // const emptyBills: Bills = [];
   const [bills, setBills] = useState([]);
   const { currUser } = state;
   useEffect(() => {
@@ -34,14 +32,49 @@ const Bills = () => {
         });
     }
   }, []);
-  console.log('here are bills: ', bills);
-  console.log('bils length: ', bills.length);
-  const data: any[] = bills;
+
+  const userPaymentSelect = ({ value }: any) => {
+    const options = [
+      { value: 'unpaid', label: 'Unpaid' },
+      { value: 'paid', label: 'Paid' },
+    ];
+
+    return <Select options={options} value={options.find(option => option.value === value)}/>;
+  };
+
+  const billPaymentSelect = ({ value }: any) => {
+    const options = [
+      { value: 'unpaid', label: 'Unpaid' },
+      { value: 'paid', label: 'Paid' },
+    ];
+
+    return <Select options={options} value={options.find(option => option.value === value)}/>;
+  };
+
   return (
     <div>
-      <h1>Bills testing testing 123</h1>
       <Heading as="h1">Bills</Heading>
-      {bills.length && <Griddle data={data} plugins={[plugins.LocalPlugin]} />}
+      {bills.length && (
+        <Griddle data={bills} plugins={[plugins.LocalPlugin]}>
+          <RowDefinition>
+            <ColumnDefinition id="name" title="Name" />
+            <ColumnDefinition id="user_amount" title="Your Amount Due" />
+            <ColumnDefinition id="total_amount" title="Total Amount Due" />
+            <ColumnDefinition id="interval" title="Interval" />
+            <ColumnDefinition id="due_date" title="Due Date" />
+            <ColumnDefinition
+              id="user_status"
+              title="Your Payment Status"
+              customComponent={userPaymentSelect}
+            />
+            <ColumnDefinition
+              id="bill_status"
+              title="Bill Payment Status"
+              customComponent={billPaymentSelect}
+            />
+          </RowDefinition>
+        </Griddle>
+      )}
     </div>
   );
 };

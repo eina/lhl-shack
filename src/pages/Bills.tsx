53d0705fbebff5eec89d232c5f00204c5c659d68
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-26 13:48:51 -0700

    change table to griddle react

--- src/pages/Bills.tsx
+++ src/pages/Bills.tsx
@@ -1,27 +1,14 @@
 import React, { useEffect, useContext, useState } from 'react';
-import { Cell, Column, Table } from '@blueprintjs/table';
-import '../../node_modules/@blueprintjs/table/lib/css/table.css';
+// import { Cell, Column, Table } from '@blueprintjs/table';
+// import '../../node_modules/@blueprintjs/table/lib/css/table.css';
+
+import Griddle, { plugins } from 'griddle-react';
 
 import { Heading } from '@chakra-ui/core';
 import axios from 'axios';
 
 import { AppContext } from '../Store';
 
-interface Bill {
-  [key: string]: any;
-}
-
-const getData = (rowIndex: number, bill: any): Bill => {
-  return bill[rowIndex];
-};
-
-const getCellRenderer = (key: string, bills: any) => {
-  return (rowIndex: number) => {
-    console.log('rowIndex: ', rowIndex);
-    const billRow = getData(rowIndex, bills);
-    return <Cell>{`${billRow[key]}`}</Cell>;
-  };
-};
 
 const Bills = () => {
   const {
@@ -33,7 +20,10 @@ const Bills = () => {
   const { currUser } = state;
   useEffect(() => {
     if (currUser) {
-      console.log('params: ', { household_id: currUser.household_id, user_id: currUser.id })
+      console.log('params: ', {
+        household_id: currUser.household_id,
+        user_id: currUser.id,
+      });
       axios
         .get('/api/bills', {
           params: { household_id: currUser.household, user_id: currUser.id },
@@ -46,34 +36,12 @@ const Bills = () => {
   }, []);
   console.log('here are bills: ', bills);
   console.log('bils length: ', bills.length);
+  const data: any[] = bills;
   return (
     <div>
       <h1>Bills testing testing 123</h1>
       <Heading as="h1">Bills</Heading>
-      {bills.length && (
-        <Table numRows={bills.length} enableRowHeader={false}>
-          <Column
-            key={1}
-            name="Name"
-            cellRenderer={getCellRenderer('name', bills)}
-          />
-          <Column
-            key={2}
-            name="Due Date"
-            cellRenderer={getCellRenderer('due_date', bills)}
-          />
-          <Column
-            key={3}
-            name="Total Amount"
-            cellRenderer={getCellRenderer('total_amount', bills)}
-          />
-          <Column
-            key={4}
-            name="Amount per person"
-            cellRenderer={getCellRenderer('user_amount', bills)}
-          />
-        </Table>
-      )}
+      {bills.length && <Griddle data={data} plugins={[plugins.LocalPlugin]} />}
     </div>
   );
 };

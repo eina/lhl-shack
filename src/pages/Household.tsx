f396694d7284d37b5eae12a0d9c0d292dc0ce5a7
Eina Onting <einers@outlook.com>
2020-03-26 23:10:31 -0700

    style household

--- src/pages/Household.tsx
+++ src/pages/Household.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 
-import { Heading, Avatar, Divider, SimpleGrid } from '@chakra-ui/core';
+import { Box, Heading, Text, Flex, Avatar, Link, SimpleGrid, Icon, Grid } from '@chakra-ui/core';
 
 import { AppContext } from '../Store';
 
@@ -68,6 +68,7 @@ const Household = () => {
   useEffect(() => {
     //get house info
     axios.get(`api/houses/${currUser.house}`).then(vals => {
+      console.log("hi", vals.data);
       setHouse(vals.data);
     });
 
@@ -97,46 +98,65 @@ const Household = () => {
       });
   }, [currUser.household]);
 
+  const FlexDLItem = ({ title, value }: { title: string; value: any }) => (
+    <Flex>
+      <Text as="dt" fontWeight="semibold" mr={1}>{title}:</Text>
+      <Text as="dd">{value}</Text>
+    </Flex>
+  );
+
   return (
     house && (
-      <div>
-        <dl>
-          <Heading as="h3">Household</Heading>
-        </dl>
-        <dd>{house.address}</dd>
-        <Divider />
-        <Heading as="h3">My Landlord</Heading>
-        <dl>
-          <dd>
-            <b>Landlord:</b> {landlord.first_name} {landlord.last_name}
-          </dd>
-          <dd>
-            <b>Phone number:</b> {landlord.phone_number}
-          </dd>
-          <dd>
-            <b>Email:</b> {landlord.email}
-          </dd>
-          <dd>
-            <b>Address:</b> {landlord.address}
-          </dd>
-        </dl>
-        <div>
-          <Divider />
-          <Heading as="h3">Roommates</Heading>
-          <SimpleGrid columns={2} spacing={2}>
+      <Box>
+        <Box as="section" mb={8}>
+          <Heading as="h3" fontSize="3xl">
+            House
+          </Heading>
+
+          <dl>
+            <FlexDLItem title="Address" value={house.address} />
+            <FlexDLItem title="Number of Rooms" value={house.number_of_rooms} />
+            <FlexDLItem title="Number of Bathrooms" value={house.number_of_bathrooms} />
+            <FlexDLItem title="Pet Friendly" value={house.pet_friendly ? "Yes" : "No"} />
+            <FlexDLItem title="Smoking Allowed" value={house.smoking_allowed ? "Yes" : "No"} />
+          </dl>
+        </Box>
+
+        <Box as="section" mb={8}>
+          <Heading as="h3" fontSize="3xl">
+            My Landlord
+          </Heading>
+          <dl>
+            <FlexDLItem title="Landlord" value={`${landlord.first_name} ${landlord.last_name}`} />
+            <FlexDLItem title="Address" value={landlord.address} />
+            {landlord.company && <FlexDLItem title="Company" value={landlord.company} />}
+            <FlexDLItem title="Phone number" value={landlord.phone_number} />
+            <FlexDLItem title="Email" value={landlord.email} />
+          </dl>
+        </Box>
+
+        <Box as="section" mb={8}>
+          <Heading as="h3" fontSize="3xl">
+            Roommates
+          </Heading>
+          <Grid templateColumns="repeat(3, 1fr)" gridGap={10}>
             {roomies.map((roomie: any) => (
-              <div key={roomie.id}>
-                <Avatar src={roomie.avatar} />
-                <p>
-                  {roomie.first_name} {roomie.last_name}
-                </p>
-                <p>{roomie.phone_number}</p>
-                <p>{roomie.email}</p>
-              </div>
+              <Flex key={roomie.id} align="center" bg="white" p={6} rounded="lg">
+                <Avatar name={`${roomie.first_name} ${roomie.last_name}`} size="xl" />
+
+                <Box ml={3} as="dl">
+                  <FlexDLItem title="Name" value={`${roomie.first_name} ${roomie.last_name}`} />
+                  <FlexDLItem
+                    title="Phone"
+                    value={<Link href={`tel: roomie.phone_number`}>{roomie.phone_number}</Link>}
+                  />
+                  <FlexDLItem title="Email" value={roomie.email} />
+                </Box>
+              </Flex>
             ))}
-          </SimpleGrid>
-        </div>
-      </div>
+          </Grid>
+        </Box>
+      </Box>
     )
   );
 };

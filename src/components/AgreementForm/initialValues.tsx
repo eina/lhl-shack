899c12ef951f16eebc5efb6d1a50ea758f75b210
Eina Onting <einers@outlook.com>
2020-03-26 20:02:34 -0700

    fix lease dates initital values

--- src/components/AgreementForm/initialValues.tsx
+++ src/components/AgreementForm/initialValues.tsx
@@ -75,181 +75,36 @@ const initialValues = {
 };
 
 // format values from DB to be usable in Formik form
-export const formatDBInitialValues = (dbInitValsObj: any) => ({
-  ...dbInitValsObj,
-  // household: {
-  //   ...dbInitValsObj.household,
-  //   leaseDates: {
-  //     startDate: moment(dbInitValsObj.household.leaseDates.startDate),
-  //     endDate: moment(dbInitValsObj.household.leaseDates.endDate)
-  //   }
-  // },
-  // rent: {
-  //   ...dbInitValsObj.rent,
-  //   due_date: moment(dbInitValsObj.rent.due_date)
-  // },
-  // securityDeposit: {
-  //   ...dbInitValsObj.securityDeposit,
-  //   due_date: moment(dbInitValsObj.securityDeposit.due_date)
-  // },
-  bills: dbInitValsObj.bills.map((bill: any) => ({
-    ...bill,
-    due_date: moment(bill.due_date)
-  })),
-  housekeeping: {
-    ...dbInitValsObj.housekeeping,
-    guestPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.guestPolicy),
-    spacesPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.spacesPolicy),
-    roomsPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.roomsPolicy),
-    choresPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.choresPolicy),
-    vacationPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.vacationPolicy),
-    personalItemsPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.personalItemsPolicy),
-    smokingPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.smokingPolicy),
-    messagesPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.messagesPolicy),
-    petsPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.petsPolicy)
-  },
-  signatures: dbInitValsObj.signatures.map((sig: any) => ({
-    ...sig,
-    date: moment(sig.dueDate)
-  }))
-});
-
-export const finishedAgreement = formatDBInitialValues(
-  JSON.parse(
-    JSON.stringify({
-      rent: {
-        name: null,
-        portion: [
-          {
-            roommate: {
-              label: "Andy Lindsay",
-              value: {
-                email: "test@test.com",
-                last_name: "Lindsay",
-                first_name: "Andy",
-                phone_number: "876-237-2519"
-              }
-            },
-            roommate_amt: "1000"
-          },
-          {
-            roommate: {
-              label: "Travis Borsa",
-              value: {
-                email: "test2@test.com",
-                last_name: "Borsa",
-                first_name: "Travis",
-                phone_number: "6048596732"
-              }
-            },
-            roommate_amt: "1000"
-          }
-        ],
-        due_date: "2020-03-24T07:15:17.741Z",
-        interval: {
-          label: "Monthly",
-          value: "monthly"
-        },
-        total_amount: "2000"
-      },
-      bills: [
-        {
-          name: "Test",
-          due_date: "2020-03-24T07:15:17.741Z",
-          interval: {
-            label: "Monthly",
-            value: "monthly"
-          },
-          total_amount: "200"
-        }
-      ],
-      roommates: [
-        {
-          email: "test@test.com",
-          last_name: "Lindsay",
-          first_name: "Andy",
-          phone_number: "876-237-2519"
-        },
-        {
-          email: "test2@test.com",
-          last_name: "Borsa",
-          first_name: "Travis",
-          phone_number: "6048596732"
-        }
-      ],
-      signatures: [
-        {
-          date: "2020-03-24T07:28:59.775Z",
-          agreed: true,
-          fullName: "Andy Lindsay"
-        },
-        {
-          date: "2020-03-24T07:28:59.775Z",
-          agreed: true,
-          fullName: "Travis Borsa"
-        }
-      ],
-      housekeeping: {
-        weekdayAM: "6",
-        weekdayPM: "12",
-        weekendAM: "6",
-        weekendPM: "12",
-        petsPolicy:
-          '{"blocks":[{"key":"6r0h3","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        guestPolicy:
-          '{"blocks":[{"key":"13dm7","text":"Marshmallow  halvah gummi bears dragée candy. Pie marzipan muffin fruitcake. Topping  dessert halvah sweet topping. Chupa chups icing ice cream caramels.","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":0,"length":11,"style":"BOLD"},{"offset":20,"length":5,"style":"ITALIC"},{"offset":77,"length":7,"style":"UNDERLINE"}],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        roomsPolicy:
-          '{"blocks":[{"key":"al2i6","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        choresPolicy:
-          '{"blocks":[{"key":"16rh6","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        spacesPolicy:
-          '{"blocks":[{"key":"7q3cn","text":"aaaaaaaaaaaaaaaaaaa","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        smokingPolicy:
-          '{"blocks":[{"key":"70o8m","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        messagesPolicy:
-          '{"blocks":[{"key":"11hkg","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        vacationPolicy:
-          '{"blocks":[{"key":"1067p","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        personalItemsPolicy:
-          '{"blocks":[{"key":"104ap","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}'
-      },
-      securityDeposit: {
-        name: null,
-        portion: [
-          {
-            roommate: {
-              label: "Andy Lindsay",
-              value: {
-                email: "test@test.com",
-                last_name: "Lindsay",
-                first_name: "Andy",
-                phone_number: "876-237-2519"
-              }
-            },
-            roommate_amt: "500"
-          },
-          {
-            roommate: {
-              label: "Travis Borsa",
-              value: {
-                email: "test2@test.com",
-                last_name: "Borsa",
-                first_name: "Travis",
-                phone_number: "6048596732"
-              }
-            },
-            roommate_amt: "500"
-          }
-        ],
-        due_date: "2020-03-24T07:15:17.741Z",
-        interval: {
-          label: "Once",
-          value: "once"
-        },
-        total_amount: "1000"
-      }
-    })
-  )
-);
+export const formatDBInitialValues = (dbInitValsObj: any) => {
+  return {
+    ...dbInitValsObj,
+    leaseDates: {
+      start_date: moment(dbInitValsObj.leaseDates.start_date),
+      end_date: moment(dbInitValsObj.leaseDates.end_date)
+    },
+    bills: dbInitValsObj.bills.map((bill: any) => ({
+      ...bill,
+      due_date: moment(bill.due_date)
+    })),
+    housekeeping: {
+      ...dbInitValsObj.housekeeping,
+      guestPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.guestPolicy),
+      spacesPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.spacesPolicy),
+      roomsPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.roomsPolicy),
+      choresPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.choresPolicy),
+      vacationPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.vacationPolicy),
+      personalItemsPolicy: stringEditorStateToContent(
+        dbInitValsObj.housekeeping.personalItemsPolicy
+      ),
+      smokingPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.smokingPolicy),
+      messagesPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.messagesPolicy),
+      petsPolicy: stringEditorStateToContent(dbInitValsObj.housekeeping.petsPolicy)
+    },
+    signatures: dbInitValsObj.signatures.map((sig: any) => ({
+      ...sig,
+      date: moment(sig.dueDate)
+    }))
+  };
+};
 
 export default initialValues;
--- src/pages/Bills.tsx
+++ src/pages/Bills.tsx
@@ -10,152 +10,74 @@ import Griddle, {
 import { Heading, useToast } from '@chakra-ui/core';
 import { AppContext } from '../Store';
 
-// const rowDataSelector = (state: any, { griddleKey }: any) => {
-//   console.log('what is the state here', state);
-//   return state
-//     .get('data')
-//     .find((rowMap: any) => rowMap.get('griddleKey') === griddleKey)
-//     .toJSON();
-// };
+const Bills = () => {
+  const { state }: { state: any } = useContext(AppContext);
+  const [bills, setBills] = useState([]);
+  // const [userBillStatus, setUserBilStats] = useState({});
+  const { currUser } = state;
+  useEffect(() => {
+    if (currUser) {
+      axios.get('/api/bills', { params: { household_id: currUser.household, user_id: currUser.id } })
+        .then(vals => setBills(vals.data));
+    }
+  }, []);
 
-const enhancedWithRowData = connect((state, props) => {
-  return {
-    // rowData will be available into MyCustomComponent
-    rowData: rowDataSelector(state, props),
+  const userPaymentSelect = ({ value }: any) => {
+    console.log('what is this', value);
+    const options = [
+      { value: "unpaid", label: "Unpaid" },
+      { value: "paid", label: "Paid" }
+    ];
+
+    return (
+      <Select
+        options={options}
+        value={options.find(option => option.value === value)}
+      />
+    );
   };
-});
 
-export const rowDataSelector = (state: any, { griddleKey }: any) => {
-  const rowIndex = state.getIn(["lookup", griddleKey.toString()]);
-  return state
-    .get("data")
-    .get(rowIndex)
-    .toJSON();
-};
+  const billPaymentSelect = ({ value }: any) => {
+    const options = [
+      { value: 'unpaid', label: 'Unpaid' },
+      { value: 'paid', label: 'Paid' },
+    ];
 
-const MakeBlueComponent = (props: any) => (
-  <div style={{ backgroundColor: "#0000FF" }}>
-    {props.value}
-    {/* {props.rowData && (
-      <small style={{ marginLeft: 5, opacity: 0.5 }}>{props.rowData.company}</small>
-    )} */}
-  </div>
-);
+    return (
+      <Select
+        options={options}
+        value={options.find(option => option.value === value)}
+      />
+    );
+  };
+  
 
-const EnhanceWithRowData = connect((state, props) => ({
-  rowData: rowDataSelector(state, props)
-}));
-
-
-const EnhancedCustomComponent = EnhanceWithRowData(MakeBlueComponent);
-
-const UserPaymentSelect = ({ value, griddleKey, rowData }: any) => {
-  const options = [
-    { value: "unpaid", label: "Unpaid" },
-    { value: "paid", label: "Paid" }
-  ];
   return (
-    <Select
-      options={options}
-      value={options.find(option => option.value === value)}
-    />
+    <div>
+      <Heading as="h1">Bills</Heading>
+      {bills.length && (
+        <Griddle data={bills} plugins={[plugins.LocalPlugin]}>
+          <RowDefinition>
+            <ColumnDefinition id="name" title="Name" />
+            <ColumnDefinition id="user_amount" title="Your Amount Due" />
+            <ColumnDefinition id="total_amount" title="Total Amount Due" />
+            <ColumnDefinition id="interval" title="Interval" />
+            <ColumnDefinition id="due_date" title="Due Date" />
+            <ColumnDefinition
+              id="user_status"
+              title="Your Payment Status"
+              customComponent={userPaymentSelect}
+            />
+            <ColumnDefinition
+              id="bill_status"
+              title="Bill Payment Status"
+              customComponent={billPaymentSelect}
+            />
+          </RowDefinition>
+        </Griddle>
+      )}
+    </div>
   );
 };
-
-class Bills extends React.Component<{}, any> {
-  static contextType = AppContext;
-
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      bills: []
-    };
-  }
-
-  componentDidMount() {
-    const { state } = this.context;
-    const { currUser } = state;
-    axios
-      .get("/api/bills", {
-        params: { household_id: currUser.household, user_id: currUser.id }
-      })
-      .then(vals => this.setState({ bills: vals.data }));
-  }
-
-  render() {
-    return (
-      <Griddle data={this.state.bills} plugins={[plugins.LocalPlugin]}>
-        <RowDefinition>
-          <ColumnDefinition id="name" title="Name" />
-          <ColumnDefinition id="user_amount" title="Your Amount Due" />
-          <ColumnDefinition id="total_amount" title="Total Amount Due" />
-          <ColumnDefinition id="interval" title="Interval" />
-          <ColumnDefinition id="due_date" title="Due Date" />
-          <ColumnDefinition
-            id="due_date"
-            title="Due Date"
-            customComponent={EnhancedCustomComponent}
-          />
-        </RowDefinition>
-      </Griddle>
-    );
-  }
-}
-
-
-// const BillsFunction = () => {
-//   const { state }: { state: any } = useContext(AppContext);
-//   const [bills, setBills] = useState([]);
-//   // const [userBillStatus, setUserBilStats] = useState({});
-//   const { currUser } = state;
-//   useEffect(() => {
-//     if (currUser) {
-//       axios.get('/api/bills', { params: { household_id: currUser.household, user_id: currUser.id } })
-//         .then(vals => setBills(vals.data));
-//     }
-//   }, []);
-
-//   const billPaymentSelect = ({ value }: any) => {
-//     const options = [
-//       { value: 'unpaid', label: 'Unpaid' },
-//       { value: 'paid', label: 'Paid' },
-//     ];
-
-//     return (
-//       <Select
-//         options={options}
-//         // value={options.find(option => option.value === value)}
-//       />
-//     );
-//   };
-
-//   return (
-//     <div>
-//       <Heading as="h1">Bills</Heading>
-//       {bills.length && (
-//         <Griddle data={bills} plugins={[plugins.LocalPlugin]}>
-//           <RowDefinition>
-//             <ColumnDefinition id="name" title="Name" />
-//             <ColumnDefinition id="user_amount" title="Your Amount Due" />
-//             <ColumnDefinition id="total_amount" title="Total Amount Due" />
-//             <ColumnDefinition id="interval" title="Interval" />
-//             <ColumnDefinition id="due_date" title="Due Date" />
-//             <ColumnDefinition
-//               id="user_status"
-//               title="Your Payment Status"
-//               customComponent={EnhancedCustomComponent}
-//             />
-//             <ColumnDefinition
-//               id="bill_status"
-//               title="Bill Payment Status"
-//               customComponent={billPaymentSelect}
-//             />
-//           </RowDefinition>
-//         </Griddle>
-//       )}
-//     </div>
-//   );
-// };
 
 export default Bills;

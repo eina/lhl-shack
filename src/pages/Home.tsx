48052c2fd9ae1cfab850b844a7279bca2dd36bcf
Eina Onting <einers@outlook.com>
2020-03-28 00:20:20 -0700

    add react-clock to display quiet time

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,53 @@
 import React, { useContext } from "react";
-import { Heading, Box, Flex, Text } from "@chakra-ui/core";
-import './Dashboard.scss';
-import manWithLaptop from '../assets/person-on-laptop.png';
+import Clock from "react-live-clock";
+import { Heading, Box, Flex, Text, Image } from "@chakra-ui/core";
+import "./Dashboard.scss";
+import manWithLaptop from "../assets/person-on-laptop.png";
 
-import { AppContext } from '../Store';
+import { AppContext } from "../Store";
 
 const Home = () => {
   const { state } = useContext(AppContext);
-  
+
   // console.log(state);
 
   return (
     <>
-      <Box w="75%" bg="brand" color="white" className="dashboard-box" d="flex" justifyContent="space-between">
+      <Box
+        bg="brand"
+        color="white"
+        className="dashboard-box hero-box"
+        d="flex"
+        justifyContent="space-between"
+      >
         <Box>
-          <Text fontFamily="montserrat" fontWeight="bold" fontSize="3xl">
+          <Text fontFamily="montserrat" fontWeight="bold" fontSize="4xl" lineHeight="shorter">
             Welcome back, Andy!
           </Text>
           <Text>Something something about something here.</Text>
         </Box>
 
-        <img src={manWithLaptop} className="person-on-laptop" />
+        <Image src={manWithLaptop} className="person-on-laptop" />
+      </Box>
+      {/* change blue to orange depending on time */}
+      <Box
+        bg="blue.100"
+        className="dashboard-box"
+        d="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Text
+          fontFamily="montserrat"
+          fontWeight="bold"
+          fontSize="5xl"
+          color="blue.900"
+          lineHeight="shorter"
+          className="dashboard-clock"
+        >
+          <Clock format={"h:mm A"} ticking={true} timezone={"US/Pacific"} />
+        </Text>
+        <Text>Quiet time has started. It will end at 8 AM.</Text>
       </Box>
       {/* <Box w="25%" bg="teal.300">
           Announcements

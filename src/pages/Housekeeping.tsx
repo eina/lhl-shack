e84bc78c70347f32e49cf733576f386d3124431c
Eina Onting <einers@outlook.com>
2020-03-28 03:11:01 -0700

    fix merge conflicts

--- src/pages/Housekeeping.tsx
+++ src/pages/Housekeeping.tsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { AppContext } from '../Store';
 import { Heading, Divider } from '@chakra-ui/core';
 import { Editor, EditorState, convertFromRaw } from 'draft-js';
-import Agreement from './Agreement';
 
 interface HousekeepingRule {
   [key: string]: any;
@@ -24,144 +23,144 @@ const Housekeeping = () => {
 
   return (
     housekeeping && (
-    <div>
-      <Heading as="h1">Housekeeping</Heading>
-      <Divider />
-      <Heading as="h3" size="lg">
+      <div>
+        <Heading as="h1">Housekeeping</Heading>
+        <Divider />
+        <Heading as="h3" size="lg">
         Guest Policy
-      </Heading>
-      {housekeeping.guestPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.guestPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.guestPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.guestPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Heading as="h3" size="lg">
         Noise Policy
-      </Heading>
-      {housekeeping.weekdayAM && (
-        <div>
-          <p>
-            <b>Weekday Quiet Times: </b>
-            {housekeeping.weekdayPM}PM - {housekeeping.weekdayAM}AM
-          </p>
-          <Divider />
-          <p>
-            <b>Weekend Quiet Times: </b>
-            {housekeeping.weekendPM}PM - {housekeeping.weekendAM}AM
-          </p>
-        </div>
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.weekdayAM && (
+          <div>
+            <p>
+              <b>Weekday Quiet Times: </b>
+              {housekeeping.weekdayPM}PM - {housekeeping.weekdayAM}AM
+            </p>
+            <Divider />
+            <p>
+              <b>Weekend Quiet Times: </b>
+              {housekeeping.weekendPM}PM - {housekeeping.weekendAM}AM
+            </p>
+          </div>
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
         Rooms Policy
-      </Heading>
-      {housekeeping.roomsPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.roomsPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
-        {' '}
+        </Heading>
+        {housekeeping.roomsPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.roomsPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
+          {' '}
         Spaces Policy
-      </Heading>
-      {housekeeping.spacesPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.spacesPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.spacesPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.spacesPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
         Chores Policy
-      </Heading>
-      {housekeeping.choresPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.choresPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.choresPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.choresPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
         Vacations Policy
-      </Heading>
-      {housekeeping.vacationPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.vacationPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.vacationPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.vacationPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
         Personal Items Policy
-      </Heading>
-      {housekeeping.personalItemsPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.personalItemsPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.personalItemsPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.personalItemsPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
         Messages Policy
-      </Heading>
-      {housekeeping.messagesPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.messagesPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.messagesPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.messagesPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
         Smoking Policy
-      </Heading>
-      {housekeeping.smokingPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.smokingPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-      <Divider />
-      <Heading as="h3" size="lg">
+        </Heading>
+        {housekeeping.smokingPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.smokingPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+        <Divider />
+        <Heading as="h3" size="lg">
         Pets Policy
-      </Heading>
-      {housekeeping.petsPolicy && (
-        <Editor
-          editorState={EditorState.createWithContent(
-            convertFromRaw(JSON.parse(housekeeping.petsPolicy))
-          )}
-          onChange={() => {}}
-          readOnly={true}
-        />
-      )}
-    </div>
-  ));
+        </Heading>
+        {housekeeping.petsPolicy && (
+          <Editor
+            editorState={EditorState.createWithContent(
+              convertFromRaw(JSON.parse(housekeeping.petsPolicy))
+            )}
+            onChange={() => {}}
+            readOnly={true}
+          />
+        )}
+      </div>
+    ));
 };
 
 export default Housekeeping;

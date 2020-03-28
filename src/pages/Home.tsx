da41191e6c3d57b22f5f10e90ab8b180d513fb3d
Eina Onting <einers@outlook.com>
2020-03-28 01:49:53 -0700

    add announcements + carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
+import { Heading, Box, Flex, Text, Image, Divider, Button } from "@chakra-ui/core";
 import Clock from "react-live-clock";
-import { Heading, Box, Flex, Text, Image } from "@chakra-ui/core";
-import "./Dashboard.scss";
-import manWithLaptop from "../assets/person-on-laptop.png";
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 import { AppContext } from "../Store";
 
+import "./Dashboard.scss";
+import manWithLaptop from "../assets/person-on-laptop.png";
+
 const Home = () => {
   const { state } = useContext(AppContext);
-
-  // console.log(state);
 
   return (
     <>
@@ -50,10 +51,45 @@ const Home = () => {
         <Text>Quiet time has started. It will end at 8 AM.</Text>
       </Box>
 
-      <Box bg="indigo.500" color="white" className="dashboard-box">
-        <Text fontFamily="montserrat" fontWeight="bold" fontSize="3xl" lineHeight="shorter">
-          Announcements
-        </Text>
+      <Box bg="cyan.300" color="cyan.900" className="dashboard-box">
+        <Heading>Announcements</Heading>
+
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          totalSlides={3}
+          isIntrinsicHeight={true}
+        >
+          <Slider>
+            <Slide index={0}>
+              <Box>
+                <Text fontFamily="montserrat" fontWeight="bold" fontSize="xl" lineHeight="shorter">
+                  New garbage days!
+                </Text>
+                <Text>
+                  Toffee tiramisu sweet. Cookie chocolate bar candy toffee brownie halvah lemon
+                  drops chocolate jelly. Jujubes toffee lemon drops cupcake gingerbread chocolate.
+                </Text>
+                <ul>
+                  <li>Brownie jelly oat cake</li>
+                  <li>
+                    tootsie roll donut cheesecake tootsie roll croissant sweet. Chupa chups
+                    chocolate brownie cheesecake macaroon tart pudding cupcake brownie. Liquorice
+                    wafer croissant.
+                  </li>
+                </ul>
+                <Divider borderColor="cyan.700" />
+                <Text>Posted on Wednesday, March 25th, 8:00 PM</Text>
+              </Box>
+            </Slide>
+            <Slide index={1}>I am the second Slide.</Slide>
+            <Slide index={2}>I am the third Slide.</Slide>
+          </Slider>
+          <Flex justify="space-between">
+            <ButtonBack>Back</ButtonBack>
+            <ButtonNext>Next</ButtonNext>
+          </Flex>
+        </CarouselProvider>
       </Box>
       {/* <Box w="25%" bg="teal.300">
           Announcements

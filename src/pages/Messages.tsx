3ed6dc1f5faff10f44704d23c6ee42383bc13a37
Eina Onting <einers@outlook.com>
2020-03-29 01:14:29 -0700

    implement messages

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { Heading, Box, Textarea, FormControl, FormErrorMessage, FormHelperText, Button } from '@chakra-ui/core';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import * as dateFns from 'date-fns';
+import { Heading, Box, Textarea, FormControl, Button, Text, Divider } from '@chakra-ui/core';
 import { Formik, useField, FormikValues } from 'formik';
+
+import { AppContext } from '../Store';
 
 const FormikTextArea = (props: any) => {
   const { name } = props;
@@ -18,24 +22,50 @@ const FormikTextArea = (props: any) => {
 };
 
 const Messages = () => {
+  const { state }: { state: any } = useContext(AppContext);
+  const { currUser } = state;
+  const [ messages, setMessages ] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/messages', { params: { household_id: currUser.household } })
+      .then(messages => {
+        setMessages(messages.data);
+      });
+  }, [currUser]);
+
   const handleSubmitMessage = (values: FormikValues, actions: any) => {
-    console.log('hi is this working', values);
+    axios.post('/api/messages', { household_id: currUser.household, author: state.fullName, message_text: values.message }).then(addedMessage => {
+      actions.setSubmitting(false);
+      actions.resetForm();
+      setMessages((prev: any) => {
+        return prev.concat([addedMessage.data]);
+      });
+    });
   };
+
   return (
     <>
-      <Heading as="h1">Messages</Heading>
-      <Box as="section">
+      <Box as="section" mb={8}>
         <Heading as="h2" fontSize="md">Write your household a message</Heading>
         <Formik initialValues={{ message: ''}} onSubmit={(values, actions) => handleSubmitMessage(values, actions)}>
-          {({values, handleSubmit}) => (
+          {({values, handleSubmit, isSubmitting}) => (
             <form onSubmit={handleSubmit}>
               <FormikTextArea name="message" placeholder="We should have pizza for dinner!" />
-              <Button type="submit" isDisabled={values.message ? false : true}>Add Message</Button>
+              <Button type="submit" isDisabled={values.message ? false : true} isLoading={isSubmitting} loadingText="Adding Message">Add Message</Button>
             </form>
           )}
         </Formik>
       </Box>
-      <Box as="section"></Box>
+      <Box as="section">
+        <Heading as="h1" fontSize="2xl">Household Messages</Heading>
+        {messages.reverse().map((message: any, index: number) => (
+          <Box as="article" key={index} px={4} py={5} mb={3} borderWidth="1px" borderRadius="1em">
+            <Text mb={4}>{message.message_text}</Text>
+            <Divider />
+            <Text fontSize="sm">Posted by {message.author} {dateFns.formatDistance(new Date(message.created_at), new Date(), { addSuffix: true })}</Text>
+          </Box>
+        ))}
+      </Box>
     </>
   );
 };
